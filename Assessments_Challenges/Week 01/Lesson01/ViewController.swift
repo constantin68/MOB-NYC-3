6f7b1b8fdec3f8011ea6204fdbcbcf2f1f6bb07a
constantin dumitrescu <constantin68@gmail.com>
2015-04-28 18:37:58 -0400

    commit to master

--- Assessments_Challenges/Week 01/Lesson01/ViewController.swift
+++ Assessments_Challenges/Week 01/Lesson01/ViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate {
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,6 +20,17 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
-}
+    // override func touchesBegan(touches: NSSet, withEvent event: UIEvent) {
+        
+        // self.view.endEditing(true)
+        
+    }
+    
+    func textFieldShouldReturn(textField: UITextField!) -> Bool {
+        
+        textField.resignFirstResponder()
+        
+        return true
+        
+    }
 
